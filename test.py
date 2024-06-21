d2f4fa952d01b42f5638f3be12c33efd47fbc175
roman <roman.cheburan@gmail.com>
2024-06-21 11:59:03 +0300

    initial

--- test.py
+++ test.py
@@ -1,12 +1,25 @@
+from redis.asyncio import Redis
 from litestar import Litestar, get
 from litestar.openapi import OpenAPIConfig
 from litestar.openapi.plugins import StoplightRenderPlugin
+
+r = Redis(host='redis', port=6379, db=0)
 
 
 @get("/")
 async def index() -> dict[str, str]:
     return {"response": "ok"}
 
+
+@get("/redis")
+async def get_redis() -> dict[str, str]:
+    # get hello from redis, if not exists, set hello to redis
+    hello = await r.get("hello")
+    if hello is None:
+        await r.set("hello", "world")
+        hello = "world"
+
+    return {"hello": hello}
 
 @get("/books/{book_id:int}")
 async def get_book(book_id: int) -> dict[str, int]:
